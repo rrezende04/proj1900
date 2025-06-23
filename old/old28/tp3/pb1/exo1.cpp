17712f3370e6911d6fa99211739aedc6c8d38d53
rrezende04 <rrezende.ca@gmail.com>
2025-06-22 21:00:55 -0400

    TP3, exo1 corrected ::: RR

--- old/old28/tp3/pb1/exo1.cpp
+++ old/old28/tp3/pb1/exo1.cpp
@@ -12,39 +12,49 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-const uint16_t frequency = 1000; // en n par secondes
-const uint16_t periodUs = 1000; // periode b en microsecondes
-const uint8_t delayResolutionUs = 10; // taille de delai
-const uint16_t numberOfCycles = 3000; // periode de 1ms donc 3 secondes = 3000 cycles
+static constexpr uint8_t  TOTAL_GRADIENT_TIME_S = 3;
+static constexpr uint8_t  DELAY_RESOLUTION_US   = 10;
+
+static constexpr uint16_t FREQUENCY_HZ     = 1000;
+static constexpr uint32_t N_US_PER_SECOND  = 1000000;
+static constexpr uint16_t PERIOD_US        = N_US_PER_SECOND / FREQUENCY_HZ; // Period (b) in microseconds
+static constexpr uint16_t NUMBER_OF_CYCLES = TOTAL_GRADIENT_TIME_S * FREQUENCY_HZ;
 //const uint8_t cycleTime = 1;
 
-void turnLedOff()        // 00
-{
-    PORTA &= ~(1 <<PA1); // 0x
-    PORTA &= ~(1 <<PA0); // x0
+void initializePorts()
+{   
+    DDRA |= (1 << PA0);
+    DDRA |= (1 << PA1);
 }
 
-void turnLedGreen()     // 01
+void turnLedOff()       
 {
-    PORTA &= ~(1 << PA1); // 0x
-    PORTA |= (1 << PA0);  // x1
+    PORTA &= ~(1 << PA1);
+    PORTA &= ~(1 << PA0);
 }
 
-void turnLedRed()        // 10
+void turnLedGreen()    
 {
-    PORTA |= (1 << PA1);   // 1x
-    PORTA &= ~(1 << PA0);  // x0
+    PORTA &= ~(1 << PA1);
+    PORTA |=  (1 << PA0); 
 }
 
-void delayUs(uint16_t Us)
+void turnLedRed()       
 {
-    uint16_t cycles = Us / delayResolutionUs; //division entiere pour trouver combien de fois executer le delai
-    for (uint16_t i = 0; i < cycles; i++) _delay_us(delayResolutionUs);
+    PORTA |=  (1 << PA1);  
+    PORTA &= ~(1 << PA0); 
 }
 
-void PWM(uint8_t dutyCyclePercent)
+void delayUs(uint16_t delayTimeUs)
 {
-    // On trouve la duree en Us de tension haute (LED allumee) et basse (LED eteinte)
+    uint16_t nCycles = delayTimeUs / DELAY_RESOLUTION_US;
+
+    for (uint16_t i = 0; i < nCycles; i++) 
+        _delay_us(DELAY_RESOLUTION_US);
+}
+
+void executePwmCycle(uint8_t dutyCyclePercent)
+{
     uint16_t onTimeUs = dutyCyclePercent * periodUs / 100;
     uint16_t offTimeUs = 1000 - onTimeUs; // complement
 
@@ -55,24 +65,22 @@ void PWM(uint8_t dutyCyclePercent)
     delayUs(offTimeUs);
 }
 
-void slowTurnLedOff()
+void turnLedOffSlowly()
 {
-    for (uint32_t cyclesPWM = numberOfCycles ; cyclesPWM > 0; cyclesPWM--)
+    for (uint32_t pwmCycles = NUMBER_OF_CYCLES ; pwmCycles > 0; pwmCycles--)
     {
-        uint8_t percentPWM = cyclesPWM * 100 / numberOfCycles; // multiplication par 100 afin d'obtenir un pourcentage
-        PWM(percentPWM);
+        uint8_t pwmPercentage = pwmCycles * 100 / NUMBER_OF_CYCLES;
+        executePwmCycle(pwmPercentage);
     }
 }
 
 int main()
 {
-    // Setup entrees et sortie
-    DDRA |= (1 << PA0) | (1 << PA1);
+    initializePorts();
     
     turnLedOff();  
-    turnLedRed(); //on allume la LED (pas obligatoire)
-    slowTurnLedOff();
-    
+    turnLedRed();
+    turnLedOffSlowly();
     
     return 0;
 }
